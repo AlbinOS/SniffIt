b78216677786d5186890af75ccd9a683ce37ac5a
Albin Gilles <gilles.albin@gmail.com>
2015-12-25 15:39:11 +0100

    Removing secret form .gitingore for now and creating new controller for tastings

--- app/controllers/tastings_controller.rb
+++ app/controllers/tastings_controller.rb
@@ -1,2 +1,13 @@
 class TastingsController < ApplicationController
+  def create
+    @wine = Wine.find(params[:wine_id])
+    @tasting = @Wine.tastings.create(tasting_params)
+    redirect_to wine_path(@wine)
+  end
+
+  private
+
+  def tasting_params
+    params.require(:tasting).permit(:taster, :body)
+  end
 end
