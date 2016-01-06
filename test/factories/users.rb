fc7243c7be6b74b0f34fefd2dd69407bd03ffd8f
Albin Gilles <gilles.albin@gmail.com>
2016-01-06 12:39:16 +0100

    [#111077050] Removing factory_girl and switching to fixtures + minitest

--- test/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.safe_email("#{first_name}.#{last_name}") }
-    password Faker::Internet.password(8)
-    password_confirmation { "#{password}" }
-  end
-end
